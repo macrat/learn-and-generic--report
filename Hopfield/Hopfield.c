3d8ce1a9cff325f6efca62dcf3b86be1208b0d67
MacRat <macrater@gmail.com>
2015-11-23 14:49:57 +0900

    ヘルプ画面のパターンIDの最大値が一つ大くなっていたバグを修正。

--- Hopfield/Hopfield.c
+++ Hopfield/Hopfield.c
@@ -190,7 +190,7 @@ int main(int argc, char *argv[]){
 	if(argc <= 2){
 		fprintf(stderr, "usage: %s [INPUT ID] [NOISE LEVEL]\n", argv[0]);
 		fprintf(stderr, "\n");
-		fprintf(stderr, "INPUT ID: input pattern ID (0 - %lu)\n", PATTERN_NUM);
+		fprintf(stderr, "INPUT ID: input pattern ID (0 - %lu)\n", PATTERN_NUM-1);
 		for(i=0; i<PATTERN_NUM; i++){
 			fprintf(stderr, "\t%d: %s\n", i, PATTERN_NAMES[i]);
 		}
